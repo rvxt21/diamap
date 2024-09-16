852809c872a93a0285a8c0b007ef5e38d41067f3
rvxt21 <anastasia1koryagina@gmail.com>
2024-09-16 16:11:40 +0300

    add django admin panel

---
+++ admin_panel_diamap/admin_panel_diamap/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Podcasts,Continents,Countries
+
+admin.site.register(Podcasts)
+admin.site.register(Countries)
+admin.site.register(Continents)
---
+++ admin_panel_diamap/admin_panel_diamap/models.py
@@ -0,0 +1,42 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Continents(models.Model):
+    continentid = models.AutoField(primary_key=True)
+    continentname = models.CharField(max_length=20, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'continents'
+
+
+class Countries(models.Model):
+    countryid = models.AutoField(primary_key=True)
+    countryname = models.CharField(max_length=100)
+    continentid = models.ForeignKey('self', models.DO_NOTHING, db_column='continentid', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'countries'
+
+
+class Podcasts(models.Model):
+    podcastid = models.AutoField(primary_key=True)
+    podcastname = models.CharField(max_length=100)
+    duration = models.CharField(max_length=20)
+    podcastdescription = models.TextField()
+    audioname = models.CharField(max_length=100)
+    photoroute = models.CharField(blank=True, null=True)
+    audioroute = models.CharField(blank=True, null=True)
+    countryid = models.ForeignKey(Countries, models.DO_NOTHING, db_column='countryid')
+
+    class Meta:
+        managed = False
+        db_table = 'podcasts'
